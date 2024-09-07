df88a2770af19aeefab1e1f71a5611c4d1a89286
Mathias <mathias.e.karlsen@gmail.com>
2024-09-08 01:02:54 +0200

    Innlevering 2

--- ng2_data_structures/data_structures.py
+++ ng2_data_structures/data_structures.py
@@ -21,43 +21,59 @@ class Node:
 class Stack:
     def __init__(self):
         """Initialize stack object, with head attribute"""
-        pass
+        self.head = None
 
     def push(self, data: Any) -> None:
         """Add new node with data to stack"""
-        pass
+        self.head = Node(data, self.head)
 
-    def peek(self) -> Node | None:
+    def peek(self) -> Any | None:
         """Return data from node on top of stack, without changing stack"""
-        pass
+        return self.head.data
 
-    def pop(self) -> Node:
+    def pop(self) -> Any:
         """Remove last added node and return its data"""
-        pass
+        if self.head == None:
+            raise IndexError("Stack is empty")
+        node = self.head
+        self.head = node.next
+        return node.data
 
 
 class Queue:
     def __init__(self):
         """Initialize queue object with head and tail"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data: Any) -> None:
         """Add node with data to queue"""
-        pass
+        new_node = Node(data)
+        if self.tail is not None:
+            self.tail.next = new_node
+        self.tail = new_node
+        if self.head is None:
+            self.head = self.tail
 
-    def peek(self) -> Node | None:
+    def peek(self) -> Any | None:
         """Return data from head of queue without changing the queue"""
-        pass
+        return self.head.data
 
-    def dequeue(self) -> Node:
+    def dequeue(self) -> Any:
         """Remove node from head of queue and return its data"""
-        pass
+        if self.head == None:
+            raise IndexError("Queue is empty")
+        node = self.head
+        self.head = node.next
+        if self.head is None:
+            self.tail = None
+        return node.data
 
 
 class EmergencyRoomQueue:
     def __init__(self):
         """Initialize emergency room queue, use heapdict as property 'queue'"""
-        pass
+        self.queue = heapdict.heapdict()
 
     def add_patient_with_priority(self, patient_name: str, priority: int) -> None:
         """Add patient name and priority to queue
@@ -66,7 +82,7 @@ class EmergencyRoomQueue:
         patient_name:   String with patient name
         priority:       Integer. Higher priority corresponds to lower-value number.
         """
-        pass
+        self.queue[patient_name] = priority
 
     def update_patient_priority(self, patient_name: str, new_priority: int) -> None:
         """Update the priority of a patient which is already in the queue
@@ -76,7 +92,7 @@ class EmergencyRoomQueue:
         new_priority:   Integer, updated priority for patient
 
         """
-        pass
+        self.queue[patient_name] = new_priority
 
     def get_next_patient(self) -> str:
         """Remove highest-priority patient from queue and return patient name
@@ -84,7 +100,8 @@ class EmergencyRoomQueue:
         # Returns:
         patient_name    String, name of patient with highest priority
         """
-        pass
+        name, priority = self.queue.popitem()
+        return name
 
 
 class BinarySearchTree:
@@ -99,7 +116,9 @@ class BinarySearchTree:
         of a binary search tree (see property binarytree.Node.is_bst ). If not, raise
         ValueError.
         """
-        pass
+        if root is not None and not root.is_bst:
+            raise ValueError("Root is not a valid BST")
+        self.root = root
 
     def insert(self, value: float | int) -> None:
         """Insert a new node into the tree (binarytree.Node object)
@@ -112,7 +131,30 @@ class BinarySearchTree:
         See https://docs.python.org/3/library/warnings.html#warnings.warn
         In the case of duplicate values, leave the tree unchanged.
         """
-        pass
+        new_node = binarytree.Node(value)
+        if self.root is None:
+            self.root = new_node
+            return
+        current_node = self.root
+        done = False
+        while not done:
+            if new_node.value == current_node.value:
+                warnings.warn("Duplicate value. Value already exists in tree")
+                done = True
+
+            elif new_node.value < current_node.value:
+                if current_node.left is None:
+                     current_node.left = new_node
+                     done = True
+                else:
+                    current_node = current_node.left
+            
+            elif new_node.value > current_node.value:
+                if current_node.right is None:
+                     current_node.right = new_node
+                     done = True
+                else:
+                    current_node = current_node.right
 
     def __str__(self) -> str | None:
         """Return string representation of tree (helper function for debugging)"""
